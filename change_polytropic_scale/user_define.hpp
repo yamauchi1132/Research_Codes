c0bf67ee8a9609413babc4f8acf363a33e06a162
yamauchi <yamauchi.toshinori1132@gmail.com>
2018-07-02 17:54:06 +0900

    add

---
+++ change_polytropic_scale/user_define.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+//////you need to change////
+double m_mul = 10.0;
+double r_mul = 4.0;
+////////////////////////////
+
+double v_mul = sqrt(m_mul/r_mul);
+
+class Particle {
+public:
+  long long int id;
+  long long int istar;
+  double mass;
+  double pos[3];
+  double vel[3];
+  double acc[3];
+  double uene;
+  double alph;
+  double alphu;
+  double dens;
+  double ksr;
+  double np;
+  double vsnd;
+  double pres;
+  double emp;
+  double divv;
+  double rotv;
+  double bswt;
+  double pot;
+  double abar;
+  double zbar;
+  double enuc;
+  double vsmx;
+  double udot;
+  double dnuc;
+  double cmps[18];
+};
