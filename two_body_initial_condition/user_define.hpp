bd08f5139c2b22fbc22827835153b905f70a57ce
yamauchi <yamauchi.toshinori1132@gmail.com>
2018-07-02 17:54:23 +0900

    fix

--- two_body_initial_condition/user_define.hpp
+++ two_body_initial_condition/user_define.hpp
@@ -4,6 +4,7 @@ double msun = 1.989e+33;
 double rsun = 695700e+5;
 double G = 6.67259e-8;
 
+//// you need to change ////
 double m1 = 10*msun;
 double r1 = 4*rsun;
 double pori_n1 = 1.5;
@@ -12,10 +13,12 @@ double m2 = 10*msun;
 double r2 = 4*rsun;
 double pori_n2 = 1.5;
 
-double r_mul = 1.5;
-double r_rel_p = r_mul * (r1 + r2);
+double r_mul = 1.0;
 double v_rel_inf = 1e+6;
 double r_rel = 10 * r1;
+////////////////////////////
+
+double r_rel_p = r_mul * (r1 + r2);
 
 class Particle {
 public:
