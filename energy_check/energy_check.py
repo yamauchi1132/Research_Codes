280a6f4a356e8c635eeba5da270b0a702ec67468
yamauchi1132 <yamauchi.toshinori1132@gmail.com>
2018-08-03 23:51:56 +0900

    up

---
+++ energy_check/energy_check.py
@@ -0,0 +1,9 @@
+import sys
+import user_define
+
+if __name__ == '__main__':
+	args = sys.argv
+	if(len(args) != 3):
+		sys.stderr.write('Error : no input file\n')
+
+
---
+++ energy_check/user_define.py
@@ -0,0 +1,29 @@
+G = 6.67259e-8;  
+
+class Particle:
+  p_id = 0
+  istar = 0
+  mass = 0
+  pos[3] = {0,0,0}
+  vel[3] = [0,0,0]
+  acc[3]
+  uene
+  dalph
+  alphu
+  dens
+  ksr
+  np
+  vsnd
+  pres
+  emp
+  divv
+  rotv
+  bswt
+  pot
+  abar
+  zbar
+  enuc
+  vsmx
+  udot
+  dnuc
+  cmps[18]
